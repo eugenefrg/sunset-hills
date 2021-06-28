2676304b0521bf44fb6fd3d6ff7218b885407380
tpl-eugenerebong <eugene261997@gmail.com>
2021-06-28 22:32:03 +0800

    to draw a tower
    also added reference to a different usage of canvas

--- src/components/mainContent/canvasContent.tsx
+++ src/components/mainContent/canvasContent.tsx
@@ -7,9 +7,11 @@ export const CanvasContent: React.FC = () => {
     y: 500,
   });
   const [ctx, setCtx] = useState<CanvasRenderingContext2D>();
+  const [towers,setTowers] = useState<{x1:number,x2:number,y1:number,y2:number}[]>([]);
   const canvas: HTMLCanvasElement | undefined = document.getElementById(
     "canvas"
   ) as HTMLCanvasElement;
+
 
   const handleCanvasSize = useCallback(() => {
     if (containerElement) {
@@ -33,17 +35,38 @@ export const CanvasContent: React.FC = () => {
     }
   }, [setCtx, canvas]);
 
+
+  const drawBoxOn = useCallback((box) => {
+    if(!ctx) return false;
+    ctx.fillStyle = "red";
+    ctx.rect(box.x1, box.y1, (box.x2 - box.x1), (box.y2 - box.y1));
+    ctx.lineWidth = box.lineWidth;
+    ctx.stroke();
+  },[ctx])
+
+  const draw = useCallback(() => {
+    if(!ctx) return false;
+    for (let i = 0; i < towers.length; i++) {
+      drawBoxOn(towers[i]);
+    }
+  },[ctx,drawBoxOn,towers])
+
   const startDrawing = useCallback(() => {
     if (ctx) {
       canvas.height = containerSize.y;
       canvas.width = containerSize.x;
-      ctx.fillStyle = "red";
-      ctx.fillRect(20, 20, 150, 100);
-      ctx.fillRect(200, 20, 150, 100);
+      draw()
+      // ctx.fillStyle = "red";
+      // ctx.fillRect(20, 20, 150, 100);
+      // ctx.fillRect(200, 20, 150, 100);
     }
-  }, [ctx,canvas,containerSize]);
+  }, [ctx,canvas,containerSize,draw]);
 
+  const generateTowers = useCallback(()=>{
+    setTowers([{x1:10,x2:40,y1:containerSize.y,y2:40}])
+  },[setTowers])
 
+  useEffect(generateTowers,[containerSize])
   useEffect(onCanvasLoad, [canvas]);
   useEffect(handleCanvasSize,[containerElement])
   useEffect(startDrawing, [ctx,containerSize]);
